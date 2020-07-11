4cad668c878b6fb939750e9dea86b8b41dddcaed
thanhmannguyen3158 <66194911+thanhmannguyen3158@users.noreply.github.com>
2020-07-11 23:23:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -314,6 +314,7 @@ function generateActionManager(canvas, scene, followCam, pins) {
             }
             window.alert("Game over! Your score is: " + score);
             endgame = true;
+            location.reload();
         }
     });
 
